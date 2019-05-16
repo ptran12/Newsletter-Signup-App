755f16ecdcddb618f8fd8b31dce2882ba05c04fd
Phi Tran <phitran.722@gmail.com>
2019-05-15 22:15:24 -0500

    post request working

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const request = require("request");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+    var firstName = req.body.first
+    var lastName = req.body.last
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email);
+
 });
 
 app.listen(3000, function(){
