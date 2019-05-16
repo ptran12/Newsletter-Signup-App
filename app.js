26943e28c367232e752cae516f0c015e19f5632a
Phi Tran <phitran.722@gmail.com>
2019-05-15 22:00:52 -0500

    sign up page runs on server

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
-})
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
